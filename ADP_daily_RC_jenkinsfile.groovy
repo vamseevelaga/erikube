7190fa83bd05cfeee56441546565c5bfefeebd62
vamseevelaga <36747491+vamseevelaga@users.noreply.github.com>
2018-08-22 15:24:45 -0700

    Update ADP_daily_RC_jenkinsfile.groovy

--- ADP_daily_RC_jenkinsfile.groovy
+++ ADP_daily_RC_jenkinsfile.groovy
@@ -11,10 +11,10 @@ pipeline {
     }
     stages {
         def release_version = ${GERRIT_REFSPEC}.split('/')
-        echo "${data.get(0)}"
-        echo "${data.get(1)}"
-        echo "${data.get(2)}"
-        echo "${data.get(3)}"
+        echo "${data.get(0)}";
+        echo "${data.get(1)}";
+        echo "${data.get(2)}";
+        echo "${data.get(3)}";
         stage('Trigger all daily testing') {
             steps {
                 parallel(

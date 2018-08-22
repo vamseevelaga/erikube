f3f250c165acdf713674e31c53c3a0cb82eda898
vamseevelaga <36747491+vamseevelaga@users.noreply.github.com>
2018-08-22 15:03:05 -0700

    Update ADP_daily_RC_jenkinsfile.groovy

--- ADP_daily_RC_jenkinsfile.groovy
+++ ADP_daily_RC_jenkinsfile.groovy
@@ -10,21 +10,20 @@ pipeline {
         CICD_DIR = "cicd"
     }
     stages {
-        def release_version = ${GERRIT_REFSPEC}.split('/')
         stage('Trigger all daily testing') {
             steps {
                 parallel(
-                        "Daily VMware Release ${data.get(3)}”: {
-                            build job: "vmware-${data.get(2)}-${data.get(3)}”
+                        'Daily VMware Release 1.3.0': {
+                            build job: 'vmware-rel-1.3.0'
                         },
-                        "Daily VMware HA Release ${data.get(3)}”: {
-                            build job: "daily-vmware-ha-${data.get(2)}-${data.get(3)}”
+                        'Daily VMware HA Release 1.3.0': {
+                            build job: 'vmware-ha-rel-1.3.0'
                         },
-                        "E2C Deploy Release ${data.get(3)}”: {
-                            build job: "daily-e2c-deploy-${data.get(2)}-${data.get(3)}”
-                        }, 
-                        "E2C Upgrade Release ${data.get(3)}”: { 
-                            build job: "daily-e2c-upgrade-${data.get(2)}-${data.get}(3)”
+                        'E2C Deploy Release 1.3.0': {
+                            build job: 'daily-e2c-deploy-rel-1.3.0'
+                        },
+                        'E2C Upgrade Release 1.1.x': {
+                            build job: 'daily-e2c-upgrade-rel-1.3.0'
                         }
                 )
             }

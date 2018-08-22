fc90f39225e80a6ee9546e9f3c6b7453c40e09dd
vamseevelaga <36747491+vamseevelaga@users.noreply.github.com>
2018-08-22 15:48:19 -0700

    Update ADP_daily_RC_jenkinsfile.groovy

--- ADP_daily_RC_jenkinsfile.groovy
+++ ADP_daily_RC_jenkinsfile.groovy
@@ -1,3 +1,7 @@
+//Dummy pipe line script
+
+
+
 pipeline {
     agent any
     options {
@@ -8,25 +12,23 @@ pipeline {
     environment {
         REPO_DIR = "$WORKSPACE"
         CICD_DIR = "cicd"
-        def release_version = "${GERRIT_REFSPEC}".split('/')
     }
     stages {
         stage('Trigger all daily testing') {
-            steps { 
+            steps {
                 parallel(
-                    println "JOB_NAME = " + System.getenv('GERRIT_REFSPEC');
-                        "Daily VMware Release data.get(3)": {
-                            build job: "vmware-${data.get(2)}-${data.get(3)}"
+                        'Daily VMware Release 1.3.0': {
+                            build job: 'vmware-rel-1.3.0'
                         },
-                        'Daily VMware HA Release data.get(3)': {
-                            build job: 'daily-vmware-ha-${data.get(2)}-${data.get(3)}'
+                        'Daily VMware HA Release 1.3.0': {
+                            build job: 'vmware-ha-rel-1.3.0'
                         },
-                        'E2C Deploy Release ${data.get(3)}': {
-                            build job: 'daily-e2c-deploy-${data.get(2)}-${data.get(3)}'
-                        }, 
-                        'E2C Upgrade Release ${data.get(3)}': { 
-                            build job: 'daily-e2c-upgrade-${data.get(2)}-${data.get}(3)'
-                       }
+                        'E2C Deploy Release 1.3.0': {
+                            build job: 'daily-e2c-deploy-rel-1.3.0'
+                        },
+                        'E2C Upgrade Release 1.1.x': {
+                            build job: 'daily-e2c-upgrade-rel-1.3.0'
+                        }
                 )
             }
         }

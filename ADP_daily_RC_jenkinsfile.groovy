b25c4ccd4e1ffff90590046a49131ca0fea372b0
vamseevelaga <36747491+vamseevelaga@users.noreply.github.com>
2018-08-22 13:55:08 -0700

    Update ADP_daily_RC_jenkinsfile.groovy

--- ADP_daily_RC_jenkinsfile.groovy
+++ ADP_daily_RC_jenkinsfile.groovy
@@ -15,39 +15,32 @@ pipeline {
                 echo 'Initial cleanup and checkout...'
                 sh 'sudo chown -R ${USER}:${USER} .'
                 deleteDir()
-                echo 'Checkout rel/1.1.x code...'
+                echo 'Checkout rel/1.3.0 code...'
                 checkout([$class: 'GitSCM', branches: [[name: '*/rel/1.3.0']], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[url: 'https://github.com/vamseevelaga/erikube.git']]])
             }
         }
         stage('Trigger all daily testing') {
             steps {
                 parallel(
-                        'Daily VMware Release 1.1.x': {
-                            build job: 'vmware-rel-1.1.x'
+                        'Daily VMware Release 1.3.0': {
+                            build job: 'vmware-rel-1.3.0'
                         },
-                        'Daily VMware HA Release 1.1.x': {
-                            build job: 'daily-vmware-ha-rel-1.1.x'
+                        'Daily VMware HA Release 1.3.0': {
+                            build job: 'daily-vmware-ha-rel-1.3.0'
                         },
-                        'E2C Deploy Release 1.1.x': {
-                            build job: 'daily-e2c-deploy-rel-1.1.x'
+                        'E2C Deploy Release 1.3.0': {
+                            build job: 'daily-e2c-deploy-rel-1.3.0'
                         },
                         'E2C Upgrade Release 1.1.x': {
-                            build job: 'daily-e2c-upgrade-rel-1.1.x'
+                            build job: 'daily-e2c-upgrade-rel-1.3.0'
                         }
                 )
-            }
-        }
-        stage('Promote RC to artifactory') {
-            steps {
-                build job: 'promote-rc-1.1.x'
             }
         }
     }
     post {
         always {
             echo 'Publish daily release info'
-            sh '$CICD_DIR/daily_release/daily_release.sh'
-            archiveArtifacts 'artifact.properties'
         }
     }
 }

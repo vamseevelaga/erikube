77d4df11aaec3c8f2ed1d6bd4b93a4f8ab32ffe2
vamseevelaga <36747491+vamseevelaga@users.noreply.github.com>
2018-08-24 13:49:15 -0700

    Update ADP_daily_RC_jenkinsfile.groovy

--- ADP_daily_RC_jenkinsfile.groovy
+++ ADP_daily_RC_jenkinsfile.groovy
@@ -14,22 +14,12 @@ pipeline {
         REPO_DIR = "$WORKSPACE"
         CICD_DIR = "cicd"
     }
-<% changeSet = build.changeSet
-if (changeSet != null) {
-hadChanges = false %>
-<h2>Changes</h2>
-<ul>
-<% changeSet.each { cs ->
-hadChanges = true
-aUser = cs.author %>
-<li>Commit <b>${cs.revision}</b> by <b><%= aUser != null ? aUser.displayName :      it.author.displayName %>:</b> (${cs.msg})
-<ul>
-<% cs.affectedFiles.each { %>
-<li class="change-${it.editType.name}"><b>${it.editType.name}</b>: ${it.path}                              </li> <%  } %> </ul>   </li> <%  }
-
- if (!hadChanges) { %>  
-  <li>No Changes !!</li>
- <%  } %>   </ul> <% } %>
+   {
+    git url: "$GIT_REPO_URL", branch: "$GIT_BRANCH"
+    echo env.GIT_COMMIT
+    echo env.GIT_BRANCH
+    echo env.GIT_REVISION
+}
     stages {
         stage('Trigger all daily testing') {
             steps {

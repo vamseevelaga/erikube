6d09095b0b431f56a79bbe311a06bd671423c473
vamseevelaga <36747491+vamseevelaga@users.noreply.github.com>
2018-08-24 14:00:11 -0700

    Update ADP_daily_RC_jenkinsfile.groovy

--- ADP_daily_RC_jenkinsfile.groovy
+++ ADP_daily_RC_jenkinsfile.groovy
@@ -14,8 +14,6 @@ pipeline {
         REPO_DIR = "$WORKSPACE"
         CICD_DIR = "cicd"
     }
-    git log --since=1.days
-    git checkout remotes/origin/rel-1.3.0
    stages {
         stage('Trigger all daily testing') {
             steps {

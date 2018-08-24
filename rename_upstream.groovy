6cb1d94da8bb256f91c47192bcb3462311515658
vamseevelaga <36747491+vamseevelaga@users.noreply.github.com>
2018-08-24 15:53:00 -0700

    Update rename_upstream.groovy

--- rename_upstream.groovy
+++ rename_upstream.groovy
@@ -1,1 +1,26 @@
-echo "hello"
+//Dummy pipe line script
+
+
+
+import hudson.model.*
+pipeline {
+    agent any
+    options {
+        timestamps()
+        skipStagesAfterUnstable()
+        timeout(time: 4, unit: 'HOURS')
+    }
+    stages {
+       
+        stage('Promote RC to artifactory') {
+            steps {
+                build job: 'promote-rc-1.3.0'
+            }
+        }
+    }
+    post {
+        always {
+            echo 'Publish daily release info'
+        }
+    }
+}
